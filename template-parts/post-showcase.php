32ce4f5b95d84224c1bddca16a10bba38580954f
allanncruz <contato@allancruz.com.br>
2021-11-19 09:29:45 -0300

    conditional for time post

--- template-parts/post-showcase.php
+++ template-parts/post-showcase.php
@@ -3,7 +3,9 @@
     <div class="content">
       <div class="badges">
         <?php echo get_the_term_list( get_the_ID(), 'films_taxonomy', ', ' ); ?>
-        <span><?php the_field('films-time'); ?></span>
+        <?php if (get_field('films-time')) { ?>
+          <span><?php the_field('films-time'); ?></span>
+        <?php } ?>
       </div>
       <h2 class="showcase-title"><?php the_title(); ?></h2>
       <a class="button button-primary" href="<?php the_permalink(); ?>">Mais informações</a>
